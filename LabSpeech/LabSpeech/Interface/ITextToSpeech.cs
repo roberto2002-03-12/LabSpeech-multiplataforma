04d9d5f48619d94d807de85aa98c4ffe3ed0683a
roberto2002-03-12 <67761057+roberto2002-03-12@users.noreply.github.com>
2022-05-05 10:21:52 -0500

    Agregar archivos de proyecto.

---
+++ LabSpeech/LabSpeech/Interface/ITextToSpeech.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabSpeech
+{
+    public interface ITextToSpeech
+    {
+        void Speak(string text);
+    }
+}
